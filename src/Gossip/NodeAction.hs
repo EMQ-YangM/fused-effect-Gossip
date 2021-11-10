a3cca2d5ab3d49af942e469ea1591573506635f6
EMQ-YangM <yangm@emqx.io>
2021-11-10 18:10:17 +0800

    add Network module

--- src/Gossip/NodeAction.hs
+++ src/Gossip/NodeAction.hs
@@ -16,11 +16,6 @@
 
 module Gossip.NodeAction where
 
-import           Codec.CBOR.Decoding          (Decoder)
-import qualified Codec.CBOR.Decoding          as CBOR hiding (Done, Fail)
-import qualified Codec.CBOR.Encoding          as CBOR
-import qualified Codec.CBOR.Read              as CBOR
-import           Codec.Serialise
 import           Control.Algebra              hiding (send)
 import           Control.Arrow                (Arrow (second))
 import           Control.Carrier.Lift
@@ -40,10 +35,7 @@ import           Control.Effect.IOClasses     (Algebra, DiffTime, IOSim,
 import           Control.Effect.Labelled      hiding (send)
 import           Control.Effect.Sum           as S
 import           Control.Monad
-import           Control.Monad.ST
 import           Data.ByteString
-import qualified Data.ByteString              as BS
-import qualified Data.ByteString.Lazy         as LBS
 import           Data.Kind
 import qualified Data.List                    as L
 import           Data.Map                     (Map)
@@ -60,80 +52,6 @@ import           System.Random                (mkStdGen)
 newtype NodeId = NodeId Int deriving (Show, Read, Eq, Ord)
 
 data SIR = I | R | S deriving (Eq, Ord, Show, Read)
-
-data Message = M0 Int
-             | M1 Double
-
-data Channel m = Channel {
-
-    -- | Write bytes to the channel.
-    --
-    -- It maybe raise exceptions.
-    --
-    send :: LBS.ByteString -> m (),
-
-    -- | Read some input from the channel, or @Nothing@ to indicate EOF.
-    --
-    -- Note that having received EOF it is still possible to send.
-    -- The EOF condition is however monotonic.
-    --
-    -- It may raise exceptions (as appropriate for the monad and kind of
-    -- channel).
-    --
-    recv :: m (Maybe LBS.ByteString)
-  }
-
-
-instance Serialise Message where
-  encode (M0 i) = CBOR.encodeListLen 2 <> CBOR.encodeWord 0 <> CBOR.encodeInt i
-  encode (M1 d) = CBOR.encodeListLen 2 <> CBOR.encodeWord 1 <> CBOR.encodeDouble d
-  decode = do
-    len <- CBOR.decodeListLen
-    tag <- CBOR.decodeWord
-    case (len, tag) of
-      (2, 0) -> M0 <$> decode
-      (2, 1) -> M1 <$> decode
-      _      -> fail $ "decode Message: unknow tag " ++ show tag
-
-decodeLs :: forall s m a.
-            MonadST m
-         => (forall b. ST s b -> m b)
-         -> Channel m
-         -> Maybe LBS.ByteString
-         -> Decoder s a
-         -> m (Either CBOR.DeserialiseFailure (a, Maybe LBS.ByteString))
-decodeLs liftST Channel{recv} trailing decoder =
-    liftST (CBOR.deserialiseIncremental decoder) >>= go (LBS.toStrict <$> trailing)
-  where
-    go :: Maybe BS.ByteString
-       -> CBOR.IDecode s a
-       -> m (Either CBOR.DeserialiseFailure (a, Maybe LBS.ByteString))
-    go Nothing (CBOR.Partial k) =
-      recv >>= liftST . k . fmap LBS.toStrict >>= go Nothing
-    go (Just bs) (CBOR.Partial k) =
-      liftST (k (Just bs)) >>= go Nothing
-    go _ (CBOR.Done trailing' _ a) | BS.null trailing'
-                                   = return (Right (a, Nothing))
-                                   | otherwise
-                                   = return (Right (a, Just $ LBS.fromStrict trailing'))
-    go _ (CBOR.Fail _ _ failure) = return $ Left failure
-
-echoServer :: forall m a.
-              MonadST m
-           => Channel m
-           -> m (a, Maybe LBS.ByteString)
-echoServer channel@Channel {send} = go Nothing
-  where
-    go :: Maybe LBS.ByteString
-       -> m (a, Maybe LBS.ByteString)
-    go trailing = do
-      res <- withLiftST $ \liftST -> decodeLs liftST channel trailing (decode @Message)
-      case res of
-        Left err -> do
-          error $ "runServer: deserialise error " ++ show err
-        Right (msg, trailing') -> do
-          send $ serialise msg
-          go trailing'
 
 data NodeState s value message
   = NodeState
