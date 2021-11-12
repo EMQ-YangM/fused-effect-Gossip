8133efc7954904ab9a9437d74ee5f640d023d812
EMQ-YangM <yangm@emqx.io>
2021-11-12 15:05:57 +0800

    add module Impl, remove useless code

--- src/Gossip/Swim/Type.hs
+++ src/Gossip/Swim/Type.hs
@@ -1,52 +1,18 @@
-{-# LANGUAGE AllowAmbiguousTypes        #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE KindSignatures             #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE NamedFieldPuns             #-}
-{-# LANGUAGE RankNTypes                 #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE TypeApplications           #-}
-{-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs            #-}
+{-# LANGUAGE KindSignatures   #-}
+{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Gossip.Swim.Type where
-import           Control.Algebra              hiding (send)
-import           Control.Arrow                (Arrow (second))
-import           Control.Carrier.Lift
-import           Control.Carrier.Random.Gen
-import           Control.Carrier.Reader
+import           Control.Algebra
 import           Control.Carrier.State.Strict
-import           Control.Effect.IOClasses     (Algebra, DiffTime, IOSim,
-                                               MonadDelay (threadDelay),
-                                               MonadFork (forkIO),
-                                               MonadST (withLiftST),
-                                               MonadSTM (STM, atomically, newTQueueIO, readTVarIO),
-                                               MonadSTMTx (TQueue_, TVar_, readTQueue, readTVar, writeTQueue, writeTVar),
-                                               MonadSay (say),
-                                               MonadTime (getCurrentTime),
-                                               MonadTimer, SimTrace,
-                                               runSimTraceST,
-                                               selectTraceEventsSay)
-import qualified Control.Effect.IOClasses     as IOC
-import           Control.Effect.Labelled      hiding (send)
-import           Control.Effect.Sum           as S
-import           Control.Monad
-import           Data.ByteString              hiding (take)
+import           Control.Effect.IOClasses     (DiffTime, MonadSTM (STM),
+                                               MonadSTMTx (TQueue_))
+import           Control.Effect.Labelled
 import           Data.Kind
-import qualified Data.List                    as L
 import           Data.Map                     (Map)
-import qualified Data.Map                     as Map
 import           Data.Set                     (Set)
-import qualified Data.Set                     as Set
-import           Data.Vector                  ((!))
-import qualified Data.Vector                  as V
-import           Gossip.Shuffle
 import           Optics
 
 data Message
